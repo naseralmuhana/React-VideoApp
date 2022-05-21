5df3cbf13a647142456e3cb6b8e06cb351d71a1c
naseralmuhana0 <naseralmuhana0@gmail.com>
2022-05-21 03:51:23 +0300

    Updated!

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyCGLfHCa3Zvt07GI2JCM4qR288D8qF0BnU",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
