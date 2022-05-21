52c03b27a17e93efa8f3b951372f1fa726d918f7
naseralmuhana0 <naseralmuhana0@gmail.com>
2022-05-21 03:57:44 +0300

    Updated!

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCGLfHCa3Zvt07GI2JCM4qR288D8qF0BnU",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  authDomain: "react-video-5e661.firebaseapp.com",
+  databaseURL: "https://react-video-5e661-default-rtdb.firebaseio.com",
+  projectId: "react-video-5e661",
+  storageBucket: "react-video-5e661.appspot.com",
+  messagingSenderId: "2630409035",
+  appId: "1:2630409035:web:bc9ec75b9a2b08165466ce",
 }
 
 // Initialize Firebase
